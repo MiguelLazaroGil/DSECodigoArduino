149a2a1b6556e58e8b8eaf6e0ddba58948f63f0a
MiguelLazaroGil <115834196+MiguelLazaroGil@users.noreply.github.com>
2024-12-13 12:13:06 +0100

    MOvidos archivos

--- codigoPracticaFinal/codigoPracticaFinal.ino
+++ codigoPracticaFinal/codigoPracticaFinal.ino
@@ -32,8 +32,28 @@ void setup() {
 }
 
 void display(long int tiempo) {
-
+    //Aritmetica modular y tal para cada numero
 }
+int botonPulsado(float voltaje){
+// Pulsador 1
+  if(voltaje >= 1.25 && voltaje <2.5)
+  {
+    return 1;
+  }
+  // Pulsador 2
+  else if(voltaje >= 2.5 && voltaje <3.75)
+  {
+    return 2;
+  }
+  // Pulsador 3
+  else if(voltaje >= 3.75 && voltaje <=5)
+  {
+    return 3;
+  }else{
+    return -1;
+  }
+}
+
 void SiguientePantalla() {
   int temp = pantallaActual;
   temp = (temp + 1) % NUM_PANTALLAS;
@@ -41,15 +61,23 @@ void SiguientePantalla() {
 }
 void loop() {
   // put your main code here, to run repeatedly:
+    // Leer el pin analógico
+  int valorA4 = analogRead(pinEntrada);
+  // Convertir a voltios el valor analógico
+  float voltaje = valorA4 * (5.0/1023.0);
+  // Mostramos los voltios en el monitor serie
+  Serial.println(voltaje);
+  int numBoton=botonPulsado(voltaje);
+    bool configurar = (botonPulsado==2);
+    bool confirmar = (botonPulsado==1);
+    bool cambiar = (botonPulsado==0);
 
   {  //cambiar de pantallas
-    if (digitalRead(botonCambioPantallas) == HIGH) {
+    if (cambiar) {
       SiguientePantalla();
     }
   }
-  {  //Actualizar Todo
-    bool configurar = digitalRead(botonConfig) == HIGH;
-    bool confirmar = digitalRead(botonConfirmar) == HIGH;
+  { 
 
     //Dos opciones de codigo:
     //Primera:
@@ -57,9 +85,9 @@ void loop() {
       pantallas[i]->loop( (i == (int)pantallaActual), configurar, configurar);
     }
     //Segunda:
-    hora.loop(pantallaActual, configurar, confirmar);
-    temporizador.loop(pantallaActual, configurar, confirmar);
-    cronometro.loop(pantallaActual, configurar, confirmar);
+    hora.loop(pantallaActual==0, configurar, confirmar);
+    temporizador.loop(pantallaActual==1, configurar, confirmar);
+    cronometro.loop(pantallaActual==2, configurar, confirmar);
     //Fin actualizar todo
   }
   {  //Display pantalla actual
