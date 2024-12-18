ebef7ee838db50c30988b249373b0e65a04733d0
MiguelLazaroGil <115834196+MiguelLazaroGil@users.noreply.github.com>
2024-12-18 12:11:10 +0100

    Update temporizador.h

--- codigoPracticaFinal/temporizador.h
+++ codigoPracticaFinal/temporizador.h
@@ -6,6 +6,7 @@ class Temporizador: public Pantalla{
    Temporizador();
    void loop(long int seconds, bool pantallaActual, bool configurar,bool confirmar);
     long int GetTiempo() {
+      if(tiempo<0)tiempo=0;
     return tiempo*60; //Para que salgan segundos  minutos
   }
 };
